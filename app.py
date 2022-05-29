01bfb214d267e6254d95187c32b90166347874af
pentius00 <pentius00@gmail.com>
2022-05-29 14:22:01 -0400

    modified contract also modify app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 import json
+#from grpc import stream_unary_rpc_method_handler
 from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
 from pinata import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
-
 
 load_dotenv("key.env")
 
@@ -19,6 +19,9 @@ w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 ################################################################################
 st.title("Art Auction Fundraiser")
 st.markdown("### Ukraine aid auction")
+
+st.image('https://media2.giphy.com/media/xT5LMESHbV1KLGMsq4/giphy.gif')
+
 st.markdown("## Welcome to our Art Auction Fundraiser, to support non-profit organizations, This market serves to raise funds for the Ukraine invation by auctioning art donated by different artist, please place a bid!!  ")
 
 
@@ -32,7 +35,7 @@ def load_contract():
         certificate_abi = json.load(f)
 
     # Set the contract address (this is the address of the deployed contract)
-    contract_address = Web3.toChecksumAddress(0x43e851d41b24048b36e9e46d3979a654fb4a509c)
+    contract_address = Web3.toChecksumAddress(0x45af12c9c96a893d10409ef48f4108c040cffdf4)
 
     # Get the contract
     contract = w3.eth.contract(
@@ -47,9 +50,37 @@ def load_contract():
 contract = load_contract()
 
 
+
 ################################################################################
-# Award Certificate
+# Register New Artwork
 ################################################################################
+st.markdown("## Register New Artwork")
+artwork_name = st.text_input("Enter the name of the artwork")
+artist_name = st.text_input("Enter the artist name")
+initial_appraisal_value = st.text_input("Enter the initial appraisal amount")
+
+# Use the Streamlit `file_uploader` function create the list of digital image file types(jpg, jpeg, or png) that will be uploaded to Pinata.
+file = st.file_uploader("Upload Artwork", type=["jpg", "jpeg", "png"])
+
+if st.button("Register Artwork"):
+    # Use the `pin_artwork` helper function to pin the file to IPFS
+    artwork_ipfs_hash = pin_artwork(artwork_name, file)
+
+    artwork_uri = f"ipfs://{artwork_ipfs_hash}"
+
+    tx_hash = contract.functions.registerArtwork(
+        address,
+        artwork_name,
+        artist_name,
+        int(initial_appraisal_value),
+        artwork_uri
+    ).transact({'from': address, 'gas': 1000000})
+    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    st.write("Transaction receipt mined:")
+    st.write(dict(receipt))
+    st.write("You can view the pinned metadata file with the following IPFS Gateway Link")
+    st.markdown(f"[Artwork IPFS Gateway Link](https://ipfs.io/ipfs/{artwork_ipfs_hash})")
+st.markdown("---")
 
 
 
@@ -60,55 +91,6 @@ if account_selection == "Artist":
     st.text_input('Enter account address')
 else:
     st.text_input('Enter account address')
-
-
-
-
-
-
-#Select_account = st.selectbox("Select Artist Account", options=accounts)
-#certificate_details = st.text_input("Certificate Details", value="Art Ownership Certification")
-
-
-
-
-def pin_cert(name, cert_file):
-    # Pin the file to IPFS with Pinata
-    ipfs_file_hash = pin_file_to_ipfs(cert_file.getvalue())
-
-    # Build a token metadata file for the artwork
-    token_json = {
-        "name": cert_name,
-        "image": ipfs_file_hash
-    }
-    json_data = convert_data_to_json(token_json)
-
-    # Pin the json to IPFS with Pinata
-    json_ipfs_hash = pin_json_to_ipfs(json_data)
-
-    return json_ipfs_hash
-
-    ################################################################################
-# Award Certificate
-################################################################################
-
-
-cert_name = st.text_input("name of the item you want to certify?: ")
-
-
-file = st.file_uploader("Upload Art", type=["jpeg", "pdf"])
-
-
-if st.button("Award Certificate"):
-    cert_ipfs_hash = pin_cert(cert_name, file)
-    cert_uri = f"ipfs://{cert_ipfs_hash}"
-    tx_hash = contract.functions.registerArt(account,cert_uri).transact({'from': account, 'gas': 1000000})
-    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-    st.write("Transaction receipt mined:")
-    st.write(dict(receipt))
-    st.write("You can view the pinned metadata file with the following IPFS Gateway Link")
-    st.markdown(f"[Certificate IPFS Gateway Link](https://ipfs.io/ipfs/{cert_ipfs_hash})")
-st.markdown("---")
 
 
 elements = st.container()
@@ -122,3 +104,59 @@ st.markdown("## place a bid! ")
 st.success("Congratulation you won the auction")
 #st.balloons()
 #if st.button("Place bid")
+
+
+
+@st.cache(allow_output_mutation=True)
+# Define the load_contract function
+def load_contract():
+
+    # Load Art Gallery ABI
+    with open(Path('./contracts/compiled/AuctionMarket_abi.json')) as f:
+        certificate_abi = json.load(f)
+
+    # Set the contract address (this is the address of the deployed contract)
+    contract_address = Web3.toChecksumAddress(0x55ebde56fd89caddce30e1a152f0eab29ce74278)
+
+    # Get the contract
+    contract = w3.eth.contract(
+        address=contract_address,
+        abi=certificate_abi
+    )
+    # Return the contract from the function
+    return contract
+
+
+# Load the contract
+second_contract = load_contract()
+
+
+
+################################################################################
+# Helper functions to pin files and json to Pinata
+################################################################################
+
+
+def pin_artwork(artwork_name, artwork_file):
+    # Pin the file to IPFS with Pinata
+    ipfs_file_hash = pin_file_to_ipfs(artwork_file)
+
+    # Build a token metadata file for the artwork
+    token_json = {
+        "name": artwork_name,
+        "image": ipfs_file_hash
+    }
+    json_data = convert_data_to_json(token_json)
+
+    # Pin the json to IPFS with Pinata
+    json_ipfs_hash = pin_json_to_ipfs(json_data)
+
+    return json_ipfs_hash
+
+
+def pin_appraisal_report(report_content):
+    json_report = convert_data_to_json(report_content)
+    report_ipfs_hash = pin_json_to_ipfs(json_report)
+    return report_ipfs_hash
+
+
